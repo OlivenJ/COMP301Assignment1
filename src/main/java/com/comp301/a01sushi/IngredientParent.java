ce3b04a9169a1f1a61d939166b6a5e8000054f31
Zhiyuan <olivenjiang@outlook.com>
2022-04-28 01:44:55 -0400

    Initial

---
+++ src/main/java/com/comp301/a01sushi/IngredientParent.java
@@ -0,0 +1,12 @@
+package com.comp301.a01sushi;
+
+public class IngredientParent {
+    private String name;
+    private double price;
+    private int calories;
+    private boolean isVegetarian;
+    private boolean hasRice;
+    private boolean hasShellfish;
+
+    public
+}
